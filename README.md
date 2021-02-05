330fa82c849c6ff8604202933e51343f879f92f1
Kevin-the-man <56621344+Kevin-the-man@users.noreply.github.com>
2021-02-05 00:53:44 -0800

    initial commit

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ Run `npm run serve` in this directory to run the project locally.
 If the project requires additional setup or if you chose to use raw HTML instead of VueJS:
 
 *Provide your documentation here*
+
+This is the final product of HW4. I'm sorry I am not able to do what I initially would like to. 
+
+As you can see, the code in this repo was largely the TA's demonstration code for this project. I tweaked the code a bit so that instead of showing the attributes for all the pokemons, I was only going to show the attributes for the fire type pokemons. My attributes are also limited to just the health and the attack data. I changed the x axis and the y axis of the scatter plot to the strength and the number instead of the relative strength and the relative number, also changing the color of the dots to make each pokemon differentiable. Again, the code is largely NOT written by myself. I refered to the TAs code as learning purpose and tweaked it. 
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,90 @@
-<!-- This file will be used to layout and connect your views -->
 <template>
   <div id="app">
-      <!-- This is only a basic setup to display your views. Use HTML and CSS to create your layout using these basic commands -->
-      <View1 />
-      <View2 />
+    <!-- This is only a basic setup to display your views. Use HTML and CSS to create your layout using these basic commands -->
+    <ScatterPlot
+      class="scatter-plot"
+      v-if="dataset"
+      chartId="poke1"
+      title="All Fire type Pokemons"
+      :dataset="dataset"
+      attribX="relStrength"
+      labelX="Strength"
+      attribY="relCatchRate"
+      labelY="Catch Rate"
+      attribColor="Name"
+      attribGlyph="isLegendary"
+      attribGlyphValNormal="False"
+      attribGlyphValSpecial="True"
+      @item-selected="pokemonSelected"
+      :width="800"
+      :height="500"
+    />
+    <BarChart 
+      class="bar-chart" 
+      v-if="datasetSelected" 
+      chartId="poke2" 
+      :title="`Health and attack about ${datasetSelected.name}`" 
+      :dataset="datasetSelected.entries" 
+      attribX="name" 
+      attribY="value"  
+      labelY="Strength"
+      :width="800"
+      :height="500" />
+    <h1 v-else class="bar-chart-info">Select a Pokemon</h1>
   </div>
 </template>
 
 <script>
-// Try to use descriptive names like 'OverviewScatterPlot' or 'DetailLineChart'
-import View1 from './components/View1.vue'
-import View2 from './components/View2.vue'
+import * as d3 from "d3";
 
+import ScatterPlot from "./components/ScatterPlot.vue";
+import BarChart from "./components/BarChart.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    View1,
-    View2
+    ScatterPlot,
+    BarChart,
+  },
+  data() {
+    return {
+      dataset: null,
+      datasetSelected: null,
+    };
+  },
+
+  created() {
+    d3.csv("/data/pokemon.csv", d3.autoType).then((data) => {
+        let fire = d3.filter(data, d => d.Type_1 == "Fire")
+        this.dataset = d3.map(fire, (d) => {
+        return Object.assign(d, {
+          relStrength: d.Total, 
+          relCatchRate: d.Catch_Rate 
+        })
+      })
+    });
+  },
+  methods: {
+    pokemonSelected(p) {
+      if (p == null) {
+        this.datasetSelected = null
+        return
+      }
+      let pokemon = d3.filter(this.dataset, d => d.Number == p.Number)[0]
+      
+      let entries = []
+      for (const stat of ['HP', 'Attack']) {
+        entries.push({
+          name: stat,
+          value: pokemon[stat]
+        })
+      }
+      this.datasetSelected = {
+        name: pokemon.Name,
+        entries: entries
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -27,7 +93,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: hsl(210, 29%, 24%);
+  display: grid;
+  grid-template-rows: 5vh 60vh auto;
+  grid-template-columns: 1fr 10fr 10fr 1fr;
+  grid-template-areas: 
+    ". . . ." 
+    ". overview detail ."
+    ". . . .";
+}
+.scatter-plot {
+  grid-area: overview;
+}
+.bar-chart {
+  grid-area: detail;
+}
+.bar-chart-info {
+  grid-area: detail;
+  color: rgb(214, 214, 214);
+  margin: auto;
 }
 </style>
